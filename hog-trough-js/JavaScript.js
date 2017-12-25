2b8dfee27b80729471dd2e4edf5ac51c21c82606
Kevin Griffin <coemgen@hotmail.com>
2017-12-25 09:48:58 -0500

    WIP

--- hog-trough-js/JavaScript.js
+++ hog-trough-js/JavaScript.js
@@ -1,101 +1,105 @@
 /*jslint browser, devel, es6, maxlen: 80, this*/
 /*global $, jQuery*/
 
-const restaurants = (function ($) {
+(function () {
     "use strict";
 
-    const restaurantsObj = {
-        "--TBD--": "https://www.google.com/",
-        "BERTUCCI'S": "http://www.bertuccis.com/",
-        "PAPA GINOS": "https://www.papaginos.com/menu"
-    };
+    const restaurants = (function ($) {
+        const restaurantsObj = {
+            "--TBD--": "https://www.google.com/",
+            "BERTUCCI'S": "http://www.bertuccis.com/",
+            "PAPA GINOS": "https://www.papaginos.com/menu"
+        };
 
-    function changeUrl() {
-        $("form#newGroupOrder a").attr("href", restaurantsObj[this.value]);
-    }
+        function changeUrl() {
+            $("form#newGroupOrder a").attr("href", restaurantsObj[this.value]);
+        }
 
-    function get() {
-        return restaurantsObj;
-    }
+        function get() {
+            return restaurantsObj;
+        }
 
-    return {
-        changeUrl,
-        get
-    };
+        return {
+            changeUrl,
+            get
+        };
 
-}(jQuery));
+    }(jQuery));
 
-const orders = (function () {
-    "use strict";
+    const orders = (function () {
+        let ordersArr = [];
 
-    let ordersArr = [];
+        function add(newOrder) {
+            ordersArr.push(newOrder);
+        }
 
-    function add(newOrder) {
-        ordersArr.push(newOrder);
-    }
+        function display(formDataObj) {
+            $("table tbody").append(
+                "<tr>" +
+                "<td>" + 1 + "</td>" +
+                "<td>" + "Griffin,Kevin" + "</td>" +
+                "<td>" + formDataObj.orderText + "</td>" +
+                "<td>" + formDataObj.orderPrice + "</td>" +
+                "<td>" + formDataObj.orderPrice * 0.07 + "</td>" +
+                "<td>" + formDataObj.orderPrice + "</td>" +
+                "</tr>"
+            );
 
-    return {
-        add
-    };
+            $("table").show();
+        }
 
-}());
+        return {
+            add,
+            display
+        };
 
-const enterEdit = (function ($, restaurants) {
-    "use strict";
+    }());
 
-    let formArr = [];
-    let formObj = {};
+    const enterEdit = (function ($, restaurants) {
+        let formArr = [];
+        let formObj = {};
 
-    function formArrToObj(total, curVal) {
-        total[curVal.name] = curVal.value;
-        return total;
-    }
+        function formArrToObj(total, curVal) {
+            total[curVal.name] = curVal.value;
+            return total;
+        }
 
-    Object.keys(restaurants.get()).forEach(function (key) {
-        $("select#restaurant").append(
-            "<option>" + key + "</option>"
-        );
+        Object.keys(restaurants.get()).forEach(function (key) {
+            $("select#restaurant").append(
+                "<option>" + key + "</option>"
+            );
+        });
+
+        function displayForm() {
+            $(function () {
+                $("button#newGroupOrder").hide();
+                $("form#newGroupOrder").show();
+                $("form#newGroupOrder").submit(function (event) {
+                    event.preventDefault();
+                    $("form#newGroupOrder").hide();
+                    $("button#newGroupOrder").show();
+                    formArr = $("form#newGroupOrder").serializeArray();
+                    formObj = formArr.reduce(formArrToObj, {});
+                    orders.display(formObj);
+                });
+                $("select#restaurant option").click(restaurants.changeUrl);
+            });
+        }
+
+        function getFormData() {
+            return formObj;
+        }
+
+        return {
+            displayForm,
+            getFormData
+        };
+
+    }(jQuery, restaurants));
+
+    // onload
+    $(function driver() {
+        $("button#newGroupOrder").click(enterEdit.displayForm);
     });
 
-    function displayForm() {
-        $(function () {
-            $("button#newGroupOrder").hide();
-            $("form#newGroupOrder").show();
-            $("form#newGroupOrder").submit(function (event) {
-                event.preventDefault();
-                $("form#newGroupOrder").hide();
-                $("button#newGroupOrder").show();
-                formArr = $("form#newGroupOrder").serializeArray();
-                formObj = formArr.reduce(formArrToObj, {});
-                $("table tbody").append(
-                    "<tr>" +
-                    "<td>" + 1 + "</td>" +
-                    "<td>" + "Griffin,Kevin" + "</td>" +
-                    "<td>" + formObj.orderText + "</td>" +
-                    "<td>" + formObj.orderPrice + "</td>" +
-                    "<td>" + formObj.orderPrice * 0.07 + "</td>" +
-                    "<td>" + formObj.orderPrice + "</td>" +
-                    "</tr>"
-                );
-                $("table").show();
-            });
-            $("select#restaurant option").click(restaurants.changeUrl);
-        });
-    }
-
-    function getFormData() {
-        return formObj;
-    }
-
-    return {
-        displayForm,
-        getFormData
-    };
-
-}(jQuery, restaurants));
-
-// onload
-$(function driver() {
-    "use strict";
-    $("button#newGroupOrder").click(enterEdit.displayForm);
-});
+}());
